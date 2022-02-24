7dd385eb1e2b05c778c3b5390a07e0c957b756ea
Shrivu Shankar <shrivu1122@gmail.com>
2022-02-24 11:01:03 -0600

    its working

--- server.js
+++ server.js
@@ -1,41 +1,49 @@
 const express = require("express");
 const app = express();
-
-let broadcaster;
-const port = 4000;
-
 const http = require("http");
-const server = http.createServer(app);
 
+const port = process.env.PORT || 4000;
+const server = http.createServer(app);
 const io = require("socket.io")(server);
+
 app.use(express.static(__dirname + "/build"));
+
+const genRandomID = () => {
+  const vocab = "ABCDEFGHJKMNPQRSTUVWXYZ23456789";
+  let result = "";
+  for (var i = 0; i < 6; i++) {
+    result += vocab.charAt(Math.floor(Math.random() * vocab.length));
+  }
+  return result;
+};
+
+const rooms = {};
 
 io.sockets.on("error", (e) => console.log(e));
 io.sockets.on("connection", (socket) => {
-  socket.on("broadcaster", () => {
-    console.log("broadcaster");
-    broadcaster = socket.id;
-    socket.broadcast.emit("broadcaster");
+  let roomID = genRandomID();
+  socket.on("share", () => {
+    console.log("share", roomID);
+    rooms[roomID] = {
+      sharerSocketID: socket.id,
+    };
+    socket.emit("roomID", roomID);
   });
-  socket.on("watcher", () => {
-    console.log("watcher");
-    socket.to(broadcaster).emit("watcher", socket.id);
+  socket.on("watch", (newRoomID) => {
+    roomID = newRoomID;
+    socket.to(rooms[roomID].sharerSocketID).emit("watch", socket.id);
   });
-  socket.on("offer", (id, message) => {
-    console.log("offer");
-    socket.to(id).emit("offer", socket.id, message);
+  socket.on("rtc:offer", (id, message) => {
+    socket.to(id).emit("rtc:offer", socket.id, message);
   });
-  socket.on("answer", (id, message) => {
-    console.log("answer");
-    socket.to(id).emit("answer", socket.id, message);
+  socket.on("rtc:answer", (id, message) => {
+    socket.to(id).emit("rtc:answer", socket.id, message);
   });
-  socket.on("candidate", (id, message) => {
-    console.log("candidate");
-    socket.to(id).emit("candidate", socket.id, message);
+  socket.on("rtc:candidate", (id, message) => {
+    socket.to(id).emit("rtc:candidate", socket.id, message);
   });
   socket.on("disconnect", () => {
-    console.log("disconnect");
-    socket.to(broadcaster).emit("disconnectPeer", socket.id);
+    socket.to(rooms[roomID].sharerSocketID).emit("rtc:disconnect", socket.id);
   });
 });
 server.listen(port, () => console.log(`Server is running on port ${port}`));
